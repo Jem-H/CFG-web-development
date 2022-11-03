8a900c572fbaf803ed43bf8b3fe35830966b4d22
jemhs <jemimahsu@yahoo.co.uk>
2022-11-03 19:22:58 +0000

    S5 JS Code along in class

---
+++ s5-js-code-along/js/app.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+
+
+// Your code goes here
+let myName = `Jemima`;
+myName = `Jem`;
+console.log(myName);
+
+
+// Exercise: FUNCTIONS
+
+// Exercise: ARGUMENTS
+
+// Passing Variables as arguments when we invoke the function
+
+// Exercise: CONTROL FLOW
+
+// ADDING LOGIC(code available on slides)
+
+// Exercise: FUNCTION LOGIC
+
+
+// SCOPE (code available on slides)
