9c41af5f1f9b3c15247ddc5c01ea7ad902e71224
jemhs <jemimahsu@yahoo.co.uk>
2022-11-09 21:36:47 +0000

    S5 HW Calculator

---
+++ s5-homework/js/calculator.js
@@ -0,0 +1,34 @@
+// Summing
+function sum(number1, number2) {   
+    console.log(number1+number2); 
+};
+
+// Subtracting
+function subtract(number1, number2) {
+    console.log(number1-number2);
+};
+
+// Dividing
+function divide(number1, number2) {
+    console.log(number1/number2);
+};
+
+// Multiplying
+function multiply(number1, number2) {
+    console.log(number1*number2);
+};
+
+// Power
+function power(number1, number2) {
+    console.log(number1**number2);
+};
+
+// Square root
+function sqrt(number1) {
+    console.log(Math.sqrt(number1));
+};
+
+
+
+
+
